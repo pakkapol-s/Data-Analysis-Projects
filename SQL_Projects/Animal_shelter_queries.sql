02e1ca081987ea4050b71e32914f2c17edcdf620
SAIPANXX <86051449+SAIPANXX@users.noreply.github.com>
2025-08-22 19:03:04 +0100

    updated create table schemas

--- SQL_Projects/Animal_shelter_queries.sql
+++ SQL_Projects/Animal_shelter_queries.sql
@@ -72,3 +72,114 @@ HAVING	MAX(V.Vaccination_Time) < '20191001'
 		MAX(V.Vaccination_Time) IS NULL
 ORDER BY	AN.Species,
 			AN.Name;
+
+-- Return an animal's species, name, checkup times, heart rate, and a Boolean column that is TRUE only 
+-- for animals which all of their heart rate measurements were either equal to, 
+-- or larger that the average heart rate for their species.
+
+-- METHOD 1 -- Using a join
+
+WITH avg_hr AS (
+-- This calculates the AVG heart rate by species
+		SELECT
+			species,
+			ROUND(AVG(heart_rate),2) AS Rounded_AVG_hr_species, -- to get rouned results
+			CAST( AVG(heart_rate) AS DECIMAL (5,2) ) AS Cast_AVG_hr_species -- We can also use CAST
+		FROM
+			routine_checkups rc
+		GROUP BY
+			species 
+			)
+-- SELECT * FROM avg_hr ; -- Test
+SELECT
+	rc.species,
+	rc."name",
+	rc.checkup_time,
+	rc.heart_rate,
+	ahr.Cast_AVG_hr_species, -- just for comparison purposes
+	ahr.Rounded_AVG_hr_species,
+	CASE
+		WHEN rc.heart_rate >= ahr.Rounded_AVG_hr_species THEN TRUE
+		ELSE FALSE
+	END AS higher_than_average 
+FROM
+	routine_checkups rc
+INNER JOIN avg_hr ahr
+	-- The join brings the table with the avg hr per species
+ ON
+	rc.species = ahr.species
+WHERE 
+	CASE
+		WHEN rc.heart_rate >= ahr.Rounded_AVG_hr_species THEN TRUE
+		ELSE FALSE
+	END = TRUE
+ORDER BY
+	rc.species ASC,
+	rc.checkup_time ASC
+;
+
+-- METHOD 2 -- Using Window funtion. 
+-- Window function and filtering only animals whose heart rate is higher than the average for it's species
+
+WITH all_values AS (
+		SELECT
+			rc.species,
+			rc."name",
+			rc.checkup_time,
+			rc.heart_rate,
+			CAST( AVG(rc.heart_rate) OVER (PARTITION BY species) AS DECIMAL (5,2) ) AS AVG_hr_species,
+			CASE
+				WHEN rc.heart_rate >= CAST( AVG(rc.heart_rate) OVER (PARTITION BY species) AS DECIMAL (5,2) ) THEN TRUE
+				ELSE FALSE
+			END AS higher_than_average
+		FROM
+			routine_checkups rc
+		ORDER BY
+			rc.species ASC,
+			rc.checkup_time ASC
+		)
+--SELECT * FROM all_values ; -- Test
+SELECT
+	*
+FROM all_values 
+WHERE 
+	higher_than_average = TRUE
+ORDER BY
+	species ASC,
+	checkup_time ASC
+;
+
+-- Chapter 5 video 4
+-- Weight analysis of animals 
+
+SELECT	species, 
+		name, 
+		CAST (AVG (weight) AS DECIMAL (5, 2)) AS average_weight
+FROM 	routine_checkups
+GROUP BY 	species, 
+			name
+ORDER BY 	species DESC, 
+			average_weight DESC;
+
+WITH average_weights
+AS
+(
+SELECT	species, 
+		name, 
+		CAST (AVG (weight) AS DECIMAL (5, 2)) AS average_weight
+FROM 	routine_checkups
+GROUP BY 	species, 
+			name
+)
+SELECT 	*,
+		PERCENT_RANK () 
+		OVER (PARTITION BY species 
+			  ORDER BY average_weight
+			 ) AS percent_rank,
+		CUME_DIST () 
+		OVER (PARTITION BY species 
+			  ORDER BY average_weight
+			 ) AS cumulative_distribtuion
+FROM 	average_weights
+ORDER BY 	species DESC, 
+			average_weight DESC;
