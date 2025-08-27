ba847e5382a05cc25cfc8cfbdd9594420d3f5430
SAIPANXX <86051449+SAIPANXX@users.noreply.github.com>
2025-08-27 02:51:14 +0100

    updated the window function file

--- Animal_shelter_SQL/Window_function.sql
+++ Animal_shelter_SQL/Window_function.sql
@@ -1,4 +1,30 @@
 -- Window FUNCTION
 
+Chapter 2 
+Video 2: Overview and filter clause
 
+-- Show the total number of animals in our shelter ever.
+
+SELECT 	species, 
+		name, 
+		primary_color, 
+		admission_date,
+		(	SELECT 	COUNT (*) 
+			FROM 	animals
+			WHERE 	admission_date >= '2017-01-01'
+		) AS number_of_animals
+FROM 	animals
+WHERE 	admission_date >= '2017-01-01'
+ORDER BY admission_date ASC;
+
+-- using over() saves more memory
+SELECT 	species,
+		name, 
+		primary_color, 
+		admission_date,
+		COUNT (*)
+		OVER () AS number_of_animals
+FROM 	animals	
+WHERE 	admission_date >= '2017-01-01'
+ORDER BY admission_date ASC;
 
